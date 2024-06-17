1e538733781f431406fe0087ae1a3cb6a5575e0b
Leunam77 <juan.ma6531@gmail.com>
2024-06-16 23:02:30 -0400

    un poco refactorizado

--- recognition.py
+++ recognition.py
@@ -4,60 +4,74 @@ from hmmlearn import hmm
 import pyaudio
 import wave
 import scipy.io.wavfile as wav
+from sklearn.preprocessing import StandardScaler
 import threading
 import os
 from joblib import load
 
 os.environ["LOKY_MAX_CPU_COUNT"] = "4"  # Reemplaza "4" con el número de núcleos que quieres usar
-def main():
-	# Cargar los modelos de HMM entrenados
+
+PALABRAS = ["abajo", "derecha", "izquierda", "arriba"]
+FORMAT = pyaudio.paInt16     
+CHANNELS = 1
+RATE = 44100
+CHUNK = RATE * 3
+WAVE_OUTPUT_FILENAME = "grabacion.wav"
+
+def cargar_modelos():
 	modelos = {}
-	for palabra in ["arriba", "abajo", "izquierda", "derecha"]:
-		modelos[palabra] = load(palabra + "_modelo.joblib")
+	for palabra in PALABRAS:
+		try:
+			modelos[palabra] = load(palabra + "_modelo.joblib")
+		except Exception as e:
+			print(f"No se pudo cargar el modelo para la palabra {palabra}: {e}")
+	return modelos
 
-	# Configurar la grabación de audio
-	FORMAT = pyaudio.paInt16
-	CHANNELS = 1
-	RATE = 44100
-	CHUNK = RATE * 2
+def grabar_audio(stream):
+	data = stream.read(CHUNK)
+	with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as waveFile:
+		waveFile.setnchannels(CHANNELS)
+		waveFile.setsampwidth(pyaudio.get_sample_size(FORMAT))
+		waveFile.setframerate(RATE)
+		waveFile.writeframes(data)
 
-	audio = pyaudio.PyAudio()
+def cargar_audio():
+        sr, audio_data = wav.read(WAVE_OUTPUT_FILENAME)
+        audio_data = audio_data.astype(float)
+        mfcc_feat = librosa.feature.mfcc(y=audio_data, sr=sr, n_mfcc=13).T	
+        return mfcc_feat
 
-	# Iniciar la grabación
-	stream = audio.open(format=FORMAT, channels=CHANNELS,
-						rate=RATE, input=True,
-						frames_per_buffer=CHUNK)
+def reconocer(modelos, stream, audio):
+    try:
+        ultima_palabra_reconocida = None
+        scaler = StandardScaler()
+        while True:
+            grabar_audio(stream)
+            mfcc_feat = cargar_audio()
+            mfcc_feat = scaler.fit_transform(mfcc_feat)
 
-	def reconocer():
-		ultima_palabra_reconocida = None  # Inicializar la variable aquí
-		while True:
-			data = stream.read(CHUNK)
-			# Guardar la grabación en un archivo .wav
-			WAVE_OUTPUT_FILENAME = "grabacion.wav"
-			waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-			waveFile.setnchannels(CHANNELS)
-			waveFile.setsampwidth(pyaudio.get_sample_size(FORMAT))  # Asegúrate de que esta línea está correctamente indentada
-			waveFile.setframerate(RATE)
-			waveFile.writeframes(data)
-			waveFile.close()
+            scores = {palabra: modelos[palabra].score(mfcc_feat) for palabra in PALABRAS}
+            palabra_reconocida = max(scores, key=scores.get)
+            if palabra_reconocida in PALABRAS and palabra_reconocida != ultima_palabra_reconocida:
+                print("Palabra reconocida:", palabra_reconocida)
+                ultima_palabra_reconocida = palabra_reconocida
+    except Exception as e:
+        print(f"Error durante el reconocimiento de voz: {e}")
+    finally:
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
 
-			# Cargar la grabación y extraer las características de MFCC
-			sr, audio_data = wav.read("grabacion.wav")  # Cambiar 'audio' por 'audio_data'
-			audio_data = audio_data.astype(float)  # Convertir los datos de audio a punto flotante
-			mfcc_feat = librosa.feature.mfcc(y=audio_data, sr=sr, n_mfcc=13)
+def main():
+    modelos = cargar_modelos()
 
-			# Usar los modelos de HMM para reconocer la palabra en la grabación
-			scores = {}
-			for palabra in ["arriba", "abajo", "izquierda", "derecha"]:
-				scores[palabra] = modelos[palabra].score(mfcc_feat.T)
-			palabra_reconocida = max(scores, key=scores.get)
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
-			if palabra_reconocida in ["arriba", "abajo", "izquierda", "derecha"] and palabra_reconocida != ultima_palabra_reconocida:
-				print("Palabra reconocida:", palabra_reconocida)
-				ultima_palabra_reconocida = palabra_reconocida
-
-	# Iniciar el reconocimiento de voz en un nuevo hilo
-	threading.Thread(target=reconocer).start()
+    try:
+        threading.Thread(target=reconocer, args=(modelos, stream, audio)).start()
+    except Exception as e:
+        print(f"Error al iniciar el reconocimiento de voz: {e}")
 
 if __name__ == "__main__":
-	main()
+    main()
